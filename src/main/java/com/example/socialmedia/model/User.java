8ba9bc4fa009e2cd4059b4f02b88ed02fba2720f
Ramya G <ramya.g3107@gmail.com>
2023-12-07 23:52:40 -0500

    first commit

---
+++ src/main/java/com/example/socialmedia/model/User.java
@@ -0,0 +1,73 @@
+package com.example.socialmedia.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity(name= "user_details")
+public class User {
+
+  protected User() {
+  }
+
+  @Id
+  @GeneratedValue
+  private int id;
+
+  @Size(min = 3,message = "Name should contain minimum of 3 characters ")
+  //@JsonProperty("user-name")
+  private String name;
+  @Past(message = "Date of birth should be in past")
+  private LocalDate dob;
+
+  @OneToMany(mappedBy = "user")
+  @JsonProperty
+  private List<Post> Posts;
+
+  public String toString() {
+    return "User{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", dob=" + dob +
+        '}';
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public LocalDate getDob() {
+    return dob;
+  }
+
+  public void setDob(LocalDate dob) {
+    this.dob = dob;
+  }
+
+  public User(Integer id, String name, LocalDate dob){
+    super();
+    this.id=id;
+    this.name=name;
+    this.dob=dob;
+
+  }
+}
